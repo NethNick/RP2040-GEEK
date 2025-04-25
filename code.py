330bdca5f5acc17383bfea3f0554d439ff66773d
Nicola Filippini <87082120+NethNick@users.noreply.github.com>
2025-04-25 20:26:51 +0200

    Update code.py and cleaned up unneeded code

--- code.py
+++ code.py
@@ -35,9 +35,6 @@ try:
     vfs = storage.VfsFat(sdcard)
     storage.mount(vfs, "/sd")
     error = "SD-Card not mounted"
-#    with open("/sd/logger.txt", "r") as f:
-#        string = f.readline()
-#        f.close()
     with open("/pay.txt", "r") as p:
        pay = p.readline().replace("\r","")
        p.close()
@@ -47,9 +44,6 @@ except OSError as exc:
     error = exc.errno
     if error == "no SD-Card":
         print(error)
-    if error == 2:
-        print("File: logger.txt missing!")
-
 
 display = board.DISPLAY
 
@@ -68,7 +62,6 @@ text_area.y = 20
 # Show it
 display.root_group = text_area
         
-
 kbd = Keyboard(usb_hid.devices)
 #layout = KeyboardLayoutUS(kbd)        
 layout = KeyboardLayout(kbd)             
@@ -107,35 +100,3 @@ print("\n\n\n\n\n\n\n\n\n\n\n")
 
 while True:
     time.sleep(86400)        
-        
-#with open("/sd/logger.txt", "w") as f:
-#    f.write("x")
-#    f.close()
-#
-
-# Display initialize
-#lcd_cs=board.GP9
-#lcd_dc=board.GP8
-#lcd_reset=board.GP12
-# Release any resources currently in use for the displays
-#displayio.release_displays()
-
-#display_bus = FourWire(spi, command=lcd_dc, chip_select=lcd_cs, reset=lcd_reset)
-#display = ST7789(display_bus, rotation=270, width=240, height=135, rowstart=40, colstart=53)
-
-# Make the display context
-#splash = displayio.Group()
-#display.root_group = splash
-
-# Make a background color fill
-#color_palette = displayio.Palette(1)
-#color_palette[0] = 0x0000ff  # Colore nero per pulire lo schermo
-#color_bitmap = displayio.Bitmap(display.width, display.height, 1)
-#bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
-#splash.append(bg_sprite)
-
-## Draw a label
-#text_area = label.Label(font=terminalio.FONT, text="WAIT", color=0xFFFF00, scale=2, line_spacing=1 )
-#text_group = displayio.Group(x=15, y=20)
-#text_group.append(text_area)  # Subgroup for text scaling
-#splash.append(text_group)
